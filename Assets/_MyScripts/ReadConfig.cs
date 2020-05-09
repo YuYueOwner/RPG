4dbf6d23146309f3a48355f280bd55aab5a10e53
YuYueOwner <673690355@qq.com>
2020-05-09 14:35:23 +0800

    添加读取文件

---
+++ Assets/_MyScripts/ReadConfig.cs
@@ -0,0 +1,18 @@
+﻿using HotFix_Project.Config;
+using UnityEngine;
+
+public class ReadConfig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConfigManager.Instance.AddConfig();
+        ActivityMainConfig activitymainList = DataTableManager.Instance.GetConfig<ActivityMainConfig>("ActivityMain");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
