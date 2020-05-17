1de0c4597d90255a56b18d9aa1262794fb408a97
YuYueOwner <673690355@qq.com>
2020-05-17 21:18:26 +0800

    拼了一部分

--- Assets/_MyScripts/Skill/SkillAttackPanel.cs
+++ Assets/_MyScripts/Skill/SkillAttackPanel.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class SkillAttackPanel : UIScene
 {
+    private UIButton Attack_Button;
+    private UIButton Defend_Button;
 
+
+    private void Awake()
+    {
+        Attack_Button = Helper.GetChild<UIButton>(this.transform, "Attack_Button");
+        Defend_Button = Helper.GetChild<UIButton>(this.transform, "Defend_Button");
+
+    }
+    protected override void Start()
+    {
+        base.Start();
+        Attack_Button.onClick.Add(new EventDelegate(Attack));
+        Defend_Button.onClick.Add(new EventDelegate(Defend));
+    }
+
+    //攻
+    private void Attack()
+    {
+
+    }
+
+    //守
+    private void Defend()
+    {
+
+    }
 }
