f902925d6f61f612b2911358b556bed5b466ada6
jgqcww521125 <1044397513@qq.com>
2020-05-19 00:58:53 +0800

    生成技能

---
+++ Assets/_MyScripts/Skill/OnCreateSkillItem.cs
@@ -0,0 +1,46 @@
+﻿using HotFix_Project.Config;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCreateSkillItem : MonoBehaviour
+{
+    private static OnCreateSkillItem Instance;
+
+    public static OnCreateSkillItem GetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = new OnCreateSkillItem();
+        }
+        return Instance;
+    }
+
+    public void OnCreateSkillItemClick(UIGrid grid, List<int> list)
+    {
+        //PropConfig 换成技能表
+        PropConfig cfgData = DataTableManager.Instance.GetConfig<PropConfig>("Prop");
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            Transform trans;
+            trans = Instantiate(Resources.Load("SkillDefendPanel_Item"), Vector3.zero, Quaternion.identity) as Transform;
+            trans.transform.SetParent(grid.transform);
+            trans.transform.localScale = Vector3.one;
+            UISprite sp = trans.transform.Find("SP_Icon").GetComponent<UISprite>();
+            BoxCollider box = trans.transform.GetComponent<BoxCollider>();
+
+            PropConfig.PropObject data = cfgData.GetListConfigElementByID(list[i]);
+            bool isHasData = data != null;
+            box.enabled = isHasData;
+            if (isHasData)//如果有数据
+            {
+                sp.spriteName = cfgData.GetListConfigElementByID(data.ItemID).ItemIcon;
+            }
+            else
+            {
+                sp.spriteName = "";
+            }
+        }
+        grid.Reposition();
+    }
+}
--- Assets/_MyScripts/Skill/SkillAttackPanel.cs
+++ Assets/_MyScripts/Skill/SkillAttackPanel.cs
@@ -1,14 +1,22 @@
-﻿public class SkillAttackPanel : UIScene
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillAttackPanel : UIScene
 {
     private UIButton Attack_Button;
     private UIButton Defend_Button;
     private UIButton Back_Button;
+    private UITable table;
+    private UIScrollView sv;
 
     private void Awake()
     {
         Attack_Button = Helper.GetChild<UIButton>(this.transform, "Attack_Button");
         Defend_Button = Helper.GetChild<UIButton>(this.transform, "Defend_Button");
         Back_Button = Helper.GetChild<UIButton>(this.transform, "Back_Button");
+
+        table = Helper.GetChild<UITable>(this.transform, "Table");
+        sv = Helper.GetChild<UIScrollView>(this.transform, "SV");
     }
     protected override void Start()
     {
@@ -36,5 +44,22 @@
         UIManager.Instance.SetVisible(UIPanelName.SceneStart_SkillAttackPanel, false);
         UIManager.Instance.SetVisible(UIPanelName.SceneStart_OpenBagPanel, true);
 
+    }
+
+
+    //生成防御技能Item
+    private void OnCreateSkillDefendItem()
+    {
+        //生成技能类型数量
+        for (int i = 0; i < 3; i++)
+        {
+            Transform trans = Instantiate(Resources.Load("SkillDefendPanel_Item"), Vector3.zero, Quaternion.identity) as Transform;
+            UIGrid grid = trans.Find("Grid").GetComponent<UIGrid>();
+            trans.Find("LB_TypeName").GetComponent<UILabel>().text = "刀";
+            List<int> list = new List<int>();//本集合存放的是该技能类型的已拥有的所有技能
+            OnCreateSkillItem.GetInstance().OnCreateSkillItemClick(grid, list);
+        }
+        table.Reposition();
+        sv.ResetPosition();
     }
 }
