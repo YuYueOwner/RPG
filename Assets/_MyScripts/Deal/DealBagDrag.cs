88bd28fa53f62b5784816d9e3f32166c91748602
YuYueOwner <673690355@qq.com>
2020-06-27 10:22:06 +0800

    交易系统右侧背包悬浮提示框

---
+++ Assets/_MyScripts/Deal/DealBagDrag.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealBagDrag : UIDragDropItem
+{
+    //鼠标悬停0.5s后显示物品详细信息，移开消失
+    void OnHover(bool isOver)
+    {
+        if (isOver)
+        {
+            StartCoroutine(Show());
+        }
+        else
+        {
+            StopAllCoroutines();
+            UIManager.Instance.SetVisible(UIPanelName.SceneStart_GoodsInfoPanel, false);
+        }
+    }
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    //0.5s显示详细信息面板
+    IEnumerator Show()
+    {
+        yield return new WaitForSeconds(0.5f);
+        int thisName;
+        if (int.TryParse(transform.name, out thisName) == false) yield return null;
+        if (thisName > 0)
+        {
+            //显示背包中物品内容
+            PlayerInfoManager.Instance.ShowItemInfo(int.Parse(transform.name));
+            //根据鼠标点击的位置显示详细信息面板
+            Vector3 worldPoint = UICamera.currentCamera.ScreenToWorldPoint(Input.mousePosition);
+            UIManager.Instance.SetVisible(UIPanelName.SceneStart_GoodsInfoPanel, true);
+            Debug.LogError(UICamera.currentCamera.ScreenToWorldPoint(Input.mousePosition).y);
+            if (UICamera.currentCamera.ScreenToWorldPoint(Input.mousePosition).x >= 0.9f)
+            {
+                if (UICamera.currentCamera.ScreenToWorldPoint(Input.mousePosition).y <= 0.1f)
+                {
+                    GoodsInfoPanel._instance.goBg_Sprite.transform.position = new Vector3(worldPoint.x - 0.4f, worldPoint.y + 0.4f, worldPoint.z);
+                }
+                else
+                {
+                    GoodsInfoPanel._instance.goBg_Sprite.transform.position = new Vector3(worldPoint.x - 0.4f, worldPoint.y - 0.4f, worldPoint.z);
+                }
+            }
+            else
+            {
+                if (UICamera.currentCamera.ScreenToWorldPoint(Input.mousePosition).y <= 0.1f)
+                {
+                    GoodsInfoPanel._instance.goBg_Sprite.transform.position = new Vector3(worldPoint.x + 0.4f, worldPoint.y + 0.4f, worldPoint.z);
+                }
+                else
+                {
+                    GoodsInfoPanel._instance.goBg_Sprite.transform.position = new Vector3(worldPoint.x + 0.4f, worldPoint.y - 0.4f, worldPoint.z);
+                }
+            }
+        }
+    }
+}
