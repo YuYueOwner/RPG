d312db28ac3417ff506f6938b9c6e0af444f9630
jgqcww521125 <1044397513@qq.com>
2020-06-27 20:08:27 +0800

    脚本上传

---
+++ Assets/_MyScripts/Deal/BagGoodsItem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BagGoodsItem : MonoBehaviour
+{
+    private bool isDownShift = false;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            isDownShift = true;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+        {
+            isDownShift = false;
+        }
+    }
+
+    void OnClick()
+    {
+        //左键从包裹中把该物品全部移动到待售卖区
+        if (UICamera.currentTouchID == -1)
+        {
+            Debug.LogError(this.name);
+            UILabel lb_num = transform.parent.GetChild(0).GetChild(0).GetComponent<UILabel>();
+            int id = 0, num = 0;
+            id = int.Parse(this.name);
+            num = int.Parse(lb_num.text);
+            DealPanel._instance.RefreshSellGoods(id, num);
+
+            transform.GetComponent<UISprite>().spriteName = "-1";
+            transform.parent.GetChild(0).GetComponent<UISprite>().spriteName = "-1";
+            lb_num.text = "0";
+            transform.name = "BagGoods_Item(Clone)";
+        }
+    }
+}
