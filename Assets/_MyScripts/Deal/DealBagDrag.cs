c0855c8abbdb372f005bb402a182e3fb9fbdb784
YuYueOwner <673690355@qq.com>
2020-06-28 11:58:49 +0800

    改拖拽的时候数量-1，如果是1个物品，则隐藏背包中的物品icon

--- Assets/_MyScripts/Deal/DealBagDrag.cs
+++ Assets/_MyScripts/Deal/DealBagDrag.cs
@@ -66,17 +66,21 @@ public class DealBagDrag : UIDragDropItem
         base.StartDragging();
         StopAllCoroutines();
         UIManager.Instance.SetVisible(UIPanelName.SceneStart_GoodsInfoPanel, false);
+
         if (this.tag == "Goods")
         {
             isMerge = DealPanel._instance.JudgeSellGoodsIdExist(int.Parse(this.name));
 
             UILabel lb_num = Helper.GetChild<UILabel>(this.transform.parent, "BagGoodsNumLabel");
             int num = 0;
+            //如果当前拖拽的格子里是有物品的
             if (int.TryParse(lb_num.text, out num) == true)
             {
-                lb_num.text = (num - 1).ToString();
-                lb_num.gameObject.SetActive((num - 1) > 1);
-                this.transform.parent.GetChild(0).GetComponent<UISprite>().gameObject.SetActive((num - 1) >= 0);
+                //拖拽的时候数量 -1， 如果物品数 < 1 隐藏脚标
+                lb_num.text = (int.Parse(lb_num.text) - 1).ToString();
+                lb_num.gameObject.SetActive(int.Parse(lb_num.text) < 1);
+                //如果当前物品是一件的话，拖拽的时候隐藏背包中的图标
+                this.transform.parent.GetChild(0).gameObject.SetActive(int.Parse(lb_num.text) > 0);
             }
         }
     }
