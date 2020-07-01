2ee81591d96e21074de1d5afaa89b4d25447ac0b
YuYueOwner <673690355@qq.com>
2020-07-01 22:38:03 +0800

    改变量名

--- Assets/_MyScripts/Deal/BagGoodsItem.cs
+++ Assets/_MyScripts/Deal/BagGoodsItem.cs
@@ -2,18 +2,18 @@
 
 public class BagGoodsItem : MonoBehaviour
 {
-    private bool isDownShift = false;
+    private bool isDownCtrl = false;
     private void Update()
     {
         if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
         {
-            isDownShift = true;
+            isDownCtrl = true;
 
         }
 
         if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
         {
-            isDownShift = false;
+            isDownCtrl = false;
         }
     }
 
@@ -26,7 +26,7 @@ public class BagGoodsItem : MonoBehaviour
         UILabel lb_num = transform.parent.GetChild(0).GetChild(0).GetComponent<UILabel>();
         num = int.Parse(lb_num.text);
         //左键从包裹中把该物品全部移动到待售卖区
-        if (isDownShift && UICamera.currentTouchID == -1)
+        if (isDownCtrl && UICamera.currentTouchID == -1)
         {
             //点击的时候隐藏详情介绍框
             UIManager.Instance.SetVisible(UIPanelName.SceneStart_GoodsInfoPanel, false);
